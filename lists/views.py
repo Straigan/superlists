e83b5bbe052229029914e9ef6cbace9e98cfd70d
Ilya Styapin <istyapin@gmail.com>
2022-08-13 00:19:10 +0500

    Переадресация после POST и показ всех элементов в шаблоне

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from cgitb import text
+from contextlib import redirect_stderr
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 def home_page(request):
     '''домашняя страница'''
-    return render(request, 'home.html', {
-            'new_item_text': request.POST.get('item_text', ''),
-        })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    
+    items = Item.objects.all()
+
+    return render(request, 'home.html', {'items': items})
